91958c15d8e3de0c033a96b5b0d818723a08b986
myt <miaoyitao@sjjyvr.com>
2021-09-27 10:18:28 +0800

    添加

---
+++ MavenWeb/src/main/java/com/atguigu/Hello.java
@@ -0,0 +1,11 @@
+package com.atguigu;
+
+/**
+ * @author myt
+ * @create 2021-09-27 9:58
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello Git!");
+    }
+}
