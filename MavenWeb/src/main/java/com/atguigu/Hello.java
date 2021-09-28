39f6c30920b4fc29f8949dd8fc823ab940806dd5
myt <miaoyitao@sjjyvr.com>
2021-09-28 09:55:18 +0800

    分支添加

--- MavenWeb/src/main/java/com/atguigu/Hello.java
+++ MavenWeb/src/main/java/com/atguigu/Hello.java
@@ -2,12 +2,12 @@ package com.atguigu;
 
 /**
  * @author myt
- * @create 2021-09-27 9:58
  */
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello Git!");
         System.out.println("更新1");
         System.out.println("创建分支");
+        System.out.println("分支添加");
     }
 }
