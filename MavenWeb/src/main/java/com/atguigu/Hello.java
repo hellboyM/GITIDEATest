6ef7ea1d89d2c058596d3b1470213b1bc8576c01
hellboyM <90235952+hellboyM@users.noreply.github.com>
2022-04-10 13:03:15 +0800

    Update Hello.java

--- MavenWeb/src/main/java/com/atguigu/Hello.java
+++ MavenWeb/src/main/java/com/atguigu/Hello.java
@@ -12,5 +12,6 @@ public class Hello {
         System.out.println("分支添加");
         System.out.println("在线编辑");
         System.out.println("在线编辑123");
+        System.out.println("远程有编辑");
     }
 }
