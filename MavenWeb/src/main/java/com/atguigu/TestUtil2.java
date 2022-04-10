63f590253a4d0c860d62c6dfb91670b6ee44beac
hellboyM <90235952+hellboyM@users.noreply.github.com>
2022-04-10 15:51:57 +0800

    Update TestUtil2.java

--- MavenWeb/src/main/java/com/atguigu/TestUtil2.java
+++ MavenWeb/src/main/java/com/atguigu/TestUtil2.java
@@ -8,5 +8,6 @@ package java.com.atguigu;
 public class TestUtil2 {
     public static void main(String[] args) {
         System.out.println(1111);
+        System.out.println("我在线上偷偷修改");
     }
 }
